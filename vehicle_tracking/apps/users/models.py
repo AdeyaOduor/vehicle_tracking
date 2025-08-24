8753d3ebf464d9662638f58423cee3c04cbf31cb
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2025-08-24 21:10:43 +0300

    Update models.py

--- vehicle_tracking/apps/users/models.py
+++ vehicle_tracking/apps/users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    NATIONAL = 'national'
+    COUNTY = 'county'
+    SUBCOUNTY = 'subcounty'
+    DRIVER = 'driver'
+    
+    USER_TYPES = [
+        (NATIONAL, 'National Admin'),
+        (COUNTY, 'County Admin'),
+        (SUBCOUNTY, 'Subcounty Admin'),
+        (DRIVER, 'Driver'),
+    ]
+    
+    user_type = models.CharField(max_length=20, choices=USER_TYPES)
+    county = models.CharField(max_length=100, blank=True, null=True)
+    subcounty = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.username} - {self.get_user_type_display()}"
